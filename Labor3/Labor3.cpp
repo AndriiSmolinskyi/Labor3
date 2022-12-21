09007b392fb8a2b5cf4bd0c8ba2f1cc6046c7d5d
AndriiSmolinskyi <awful.braid@gmail.com>
2022-12-21 02:06:52 +0200

    Labor3
    Labor3

---
+++ Labor3/Labor3.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+
+    cout << "Створимо прямокутник" << endl;
+    cout << "Задайте розміри" << endl;
+    int weight, high;
+    cout << "Ширина ";
+    cin >> weight;
+    cout << "Висота ";
+    cin >> high;
+    cout << endl;
+
+    for (int i = 0; i < high; i++)
+    {
+
+        for (int j = 0; j < weight; j++)
+        {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+
+}
